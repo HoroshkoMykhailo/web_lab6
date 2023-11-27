3717d51a99f3d0364f14e972c1cb3330e235994d
HoroshkoMykhailo <goroshkomihajlo@gmail.com>
2023-11-27 18:56:54 +0200

    add_toasts

--- script1.js
+++ script1.js
@@ -3,14 +3,23 @@ function ifenter(e){
         e.preventDefault();
     }
 }
-document.body.onkeydown = ifenter;
 let list = [];
+document.addEventListener("DOMContentLoaded", async function () {
+    await load();
+    document.getElementById("add_object").onclick = add;
+});
+async function load(){
+    let response = await fetch("read_from_file.php");
+    list = await response.json();
+}
+document.body.onkeydown = ifenter;
 function add(){
     var textarea = document.getElementById("input_content")
     var text = textarea.value;
     list.push(text);
     textarea.value = '';
     save_to_server(list);
+    alert(list);
 }
 async function save_to_server(list){
     let to_save = JSON.stringify(list);
@@ -19,4 +28,3 @@ async function save_to_server(list){
         body: to_save
     })
 }
-document.getElementById("add_object").onclick = add;
--- script2.js
+++ script2.js
@@ -1,6 +1,40 @@
 async function load(){
     let response = await fetch("read_from_file.php");
-    let list = await response.json();
-    alert(list);
+    list = await response.json();
 }
-load();
+async function save_to_server(list){
+    let to_save = JSON.stringify(list);
+    fetch("save_to_file.php", {
+        method: "POST",
+        body: to_save
+    })
+}
+function addToast(index) {
+
+    var toast = document.createElement('div');
+    toast.className = 'toast';
+
+
+    var closeButton = document.createElement('button');
+    closeButton.className = 'close-button';
+    closeButton.innerHTML = '&times;'; // Use '×' for a close symbol
+    closeButton.onclick = function() {
+      document.body.removeChild(toast);
+      list.slice(index, 1);
+      save_to_server(list);
+    };
+
+    toast.innerText = "<p>" + list[index] + "</p>";
+
+    toast.appendChild(closeButton);
+
+    var toasts = document.querySelector(".toasts");
+    toasts.appendChild(toast);
+}
+let list = [];
+document.addEventListener("DOMContentLoaded", async function () {
+    await load();
+    for (var i = 0; i < list.length; i++){
+        addToast(i);
+    }
+});
